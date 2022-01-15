628e6fc42ccec820e46f65b52689af4964639739
iknowmagic <ilan@iknowmagic.net>
2022-01-15 12:51:52 -0800

    init

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import { Router, Link, Route } from 'svelte-routing'
+
+  let url = ''
 </script>
 
 <header />
 <nav />
 <main class="font-spartan" />
 <footer />
+
+<Router {url}>
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="mercury">About</Link>
+    <Link to="blog">Blog</Link>
+  </nav>
+  <div>
+    <Route path="/">Home</Route>
+    <Route path="/mercury">Mercury</Route>
+  </div>
+</Router>
 
 <style>
   @import url('https://fonts.googleapis.com/css2?family=Antonio:wght@500&family=Spartan:wght@400;700&display=swap');
